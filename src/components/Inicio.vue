a6fb73cbd3dd99acdedbcdcfc7b84277ed4e646f
Luis Felipe Goulart <luisfelipegoulart94@gmail.com>
2020-09-01 23:10:58 -0300

    Ajustes do input

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,35 +1,41 @@
 <template>
   <div id="content">
     <form autocomplete="off" @submit="calcular">
+
       <div class="inputRadio">
         <input type="radio" name="tipoEntrada" id="manual" value="manual" v-model="tipoEntrada">
         <label for="manual">Manual</label>
       </div>
+
       <div class="inputRadio">
         <input type="radio" name="tipoEntrada" id="arquivo" value="arquivo"  v-model="tipoEntrada">
         <label for="arquivo">Arquivo</label>
       </div>
+
       <div id="inputDado">
-        <textarea v-if="tipoEntrada === 'manual'" v-model="dados.lista" type="text" id="arquivo"></textarea>
-        <input v-if="tipoEntrada === 'arquivo'" v-on:change="dados.lista" type="file" id="arquivo" accept=".csv">
+        <textarea v-if="tipoEntrada === 'manual'" v-model="dadosManual" type="text" id="arquivo"></textarea>
+        <input v-if="tipoEntrada === 'arquivo'" v-on:change="pegarArquivo" type="file" id="arquivo" accept=".csv">
       </div>
+
       <input type="text" name="Variavel" id="variavel" placeholder="Nome da Variavel">
-      <select v-model="tipoVariavel">
+
+      <select v-model="entradaDados.tipoVar">
         <option disabled> Selecione o Tipo de Variável</option>
         <option>Qualitativa Nominal</option>
         <option>Qualitativa Ordinal</option>
         <option>Quantitativa Discreta</option>
         <option>Quantitativa Continua</option>
       </select>
-      <select name="calculo" v-model="tipoCalculo">
+
+      <select name="calculo" v-model="entradaDados.tipoCalc">
         <option disabled>Selecione o Tipo de Calculo</option>
         <option value="amostra">Amostra</option>
         <option value="populacao">População</option>
       </select>
-      <input type="submit" value="Calcular">
 
+      <input type="submit" value="Calcular">
     </form>
-    {{ dados.lista}}
+
   </div>
 </template>
 
@@ -37,35 +43,37 @@
 export default {
   data () {
     return {
-      dados: {lista: ''},
+      dadosManual: '',
+      dadosArq: '',
+      entradaDados: {
+        vetorVariaveis: [null],
+        nomeVar: null,
+        tipoVar: 'Selecione o Tipo de Variável',
+        tipoCalc: 'Selecione o Tipo de Calculo',
+      },
       tipoEntrada: '',
-      tipoVariavel: 'Selecione o Tipo de Variável',
-      tipoCalculo: 'Selecione o Tipo de Calculo'
     }
   },
   methods : {
-    calcular () {
-      if(this.tipoEntrada === 'arquivo') {
+    pegarArquivo () {
         const arquivo = event.target.files[0]
         let reader = new FileReader();
-  
+
         reader.onload = () => {
-          this.dados.lista = reader.result.split('\n')
-          console.log(this.dados.lista);
+          this.dadosArq = reader.result.split('\n')
+          this.entradaDados.vetorVariaveis = this.dadosArq.filter(dado => dado !== '')
         }
         reader.readAsText(arquivo)
-      }
       },
-    limparDados () {
-      const listaDados = this.dados.lista.filter(dado => dado !== '')
-      console.log(listaDados);
+    calcular() {
+      
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scooped>
   div #content {
       display: flex;
       width: 100vw;
@@ -75,11 +83,8 @@ export default {
       align-items: center;
   }
 
-  h1 {
-      font-size: 40pt;
-      color: #222;
-      font-family: sans-serif;
-      text-transform: uppercase;
-      text-align: center;
+  select {
+    border: none;
+    background: #eee;
   }
 </style>
