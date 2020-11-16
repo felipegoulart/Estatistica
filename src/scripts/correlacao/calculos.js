9c4a2088a70ba93058ce2c90be9591ff05f5d520
Thales <thalesaraujobs@gmail.com>
2020-11-16 20:21:32 -0300

    '

--- src/scripts/correlacao/calculos.js
+++ src/scripts/correlacao/calculos.js
@@ -1,10 +1,11 @@
 // Declaração de variaveis
-let x = [3,2,-1,4] // Entrada de dados 
-let y = [7,5,-1,9] // Entrada de dados 
+let x = [] // Entrada de dados 
+let y = [] // Entrada de dados 
+let coef_angular = null //Coeficiente angular usado no calculo da equação da reta, ele é fornecido pelo usuário
 let auxliar_x = []
 let auxiliar_y = []
 let auxiliar_xy = []
-let soma_x, soma_y, soma_x2, soma_y2, soma_xy, n, r, correlacao, a, b, regressao, coef_angular
+let soma_x, soma_y, soma_x2, soma_y2, soma_xy, n, r, correlacao, a, b, regressao
 
 //Calculo do somatório das colunas x e y
 soma_x = x.reduce((a,b) => a + b)
